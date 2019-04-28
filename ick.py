c8912d73cc79aab577b93a66fe0691c37f2f1d13
nsuave <nsuave@nsuaves-mbp.lan>
2019-04-28 15:38:54 -0500

    Functions

--- ick.py
+++ ick.py
@@ -19,134 +19,159 @@ mailUrl = "https://api.mailgun.net/v3/" + mailSandbox + "/messages"
 ### FIND LATEST CHAPTER FROM ALL AVAILABLE CHAPTERS
 ###
 
-# GET the HTML of the Chapters page and create bs4 soup object
-reqChapters = requests.get(domain + series)
-dataChapters = reqChapters.text
-soupChapters = BeautifulSoup(dataChapters, features="html.parser")
+def findLatest(domain, series):
+    # GET the HTML of the Chapters page and create bs4 soup object
+    reqChapters = requests.get(domain + series)
+    dataChapters = reqChapters.text
+    soupChapters = BeautifulSoup(dataChapters, features="html.parser")
 
-# Define empty list
-chapterList = []
+    # Define empty list
+    chapterList = []
 
-# Find all links in page from bs4 soup, then append to list
-for path in soupChapters.find_all('a', href=True):
-    chapterList.append(path['href'])
+    # Find all links in page from bs4 soup, then append to list
+    for path in soupChapters.find_all('a', href=True):
+        chapterList.append(path['href'])
 
-# Only include the links that match the series name
-matchingURI = [s for s in chapterList if "r/" + series in s]
+    # Only include the links that match the series name
+    matchingURI = [s for s in chapterList if "r/" + series in s]
 
-# Create variable of the latest chapter URL
-latestChapter = baseDomain + matchingURI[0]
+    # Create variable of the latest chapter URL
+    latestChapter = baseDomain + matchingURI[0]
+    return matchingURI, latestChapter
 
 ###
 ### GET ALL URL'S VIA LATEST CHAPTER PAGE
 ###
 
-reqLatestChapter = requests.get(latestChapter)
-dataLatestChapter = reqLatestChapter.text
-soupLatestChapter = BeautifulSoup(dataLatestChapter, features="html.parser")
+def findAllUrl(latestChapter, matchingURI):
+    reqLatestChapter = requests.get(latestChapter)
+    dataLatestChapter = reqLatestChapter.text
+    soupLatestChapter = BeautifulSoup(dataLatestChapter, features="html.parser")
 
-chapterAllLinks = []
+    chapterAllLinks = []
 
-# Get the Base URI via chopping off the /# at the end of the matching URI
-hold = matchingURI[0]
-latestChapterBaseURI = hold[:-2]
+    # Get the Base URI via chopping off the /# at the end of the matching URI
+    hold = matchingURI[0]
+    latestChapterBaseURI = hold[:-2]
 
-#chapterLinks = soupLatestChapter.select("a[href*=next]")
+    #chapterLinks = soupLatestChapter.select("a[href*=next]")
 
-for path in soupLatestChapter.find_all('a', href=True):
-    chapterAllLinks.append(path['href'])
+    for path in soupLatestChapter.find_all('a', href=True):
+        chapterAllLinks.append(path['href'])
 
-# Match based on baseURI
-chapterAllURI = [s for s in chapterAllLinks if latestChapterBaseURI in s]
+    # Match based on baseURI
+    chapterAllURI = [s for s in chapterAllLinks if latestChapterBaseURI in s]
 
-# Prepend the base domain so we have a full URL
-chapterURLListRaw = [baseDomain + "{0}".format(i) for i in chapterAllURI]
+    # Prepend the base domain so we have a full URL
+    chapterURLListRaw = [baseDomain + "{0}".format(i) for i in chapterAllURI]
 
-# Removing duplicates
-chapterURLList = list(OrderedDict.fromkeys(chapterURLListRaw))
+    # Removing duplicates
+    chapterURLList = list(OrderedDict.fromkeys(chapterURLListRaw))
+
+    return chapterURLList
 
 ###
 ### SCRAPE EACH URL FOR THEIR IMAGE URL'S
 ###
-imageURLS = []
 
-for url in chapterURLList:
-    # GET the HTML of the current page and create bs4 soup object
-    reqImage = requests.get(url)
-    dataImage = reqImage.text
-    soupImage = BeautifulSoup(dataImage, features="html.parser")
+def findUrl(chapterURLList):
+    imageURLS = []
 
-    # Find all images in current page from bs4 soup, then append src to list
-    for img in soupImage.find_all('img'):
-        imageURLS.append(img.get('src'))
+    for url in chapterURLList:
+        # GET the HTML of the current page and create bs4 soup object
+        reqImage = requests.get(url)
+        dataImage = reqImage.text
+        soupImage = BeautifulSoup(dataImage, features="html.parser")
 
-# Prepend https:
-imageURLS = ["https:{0}".format(i) for i in imageURLS]
+        # Find all images in current page from bs4 soup, then append src to list
+        for img in soupImage.find_all('img'):
+            imageURLS.append(img.get('src'))
+
+    # Prepend https:
+    imageURLS = ["https:{0}".format(i) for i in imageURLS]
+    return imageURLS
 
 ###
 ### DOWNLOAD ALL IMAGES
 ###
 
-# Include last 8 characters from the last / to get chapter number
-latestChapterNumber = latestChapter[latestChapter.rfind("/")-8:]
+def downloadImages(latestChapter, series, imageURLS):
+    # Include last 8 characters from the last / to get chapter number
+    latestChapterNumber = latestChapter[latestChapter.rfind("/")-8:]
 
-# Only including the first 3 digits to get the last chapter number
-latestChapterNumber = latestChapterNumber[:3]
+    # Only including the first 3 digits to get the last chapter number
+    latestChapterNumber = latestChapterNumber[:3]
 
-# Create directory based on series and chapter number
-directory = "/tmp/" + series + "_" + latestChapterNumber + "/"
+    # Create directory based on series and chapter number
+    directory = "/tmp/" + series + "_" + latestChapterNumber + "/"
 
-# But first, check to see if it already exists
-if os.path.exists(directory) == True:
-    print("Directory " + directory + " already exists!")
-    exit(1)
-else:
-    os.mkdir(directory)
-    print(directory)
+    # But first, check to see if it already exists
+    if os.path.exists(directory) == True:
+        print("Directory " + directory + " already exists!")
+        exit(1)
+    else:
+        os.mkdir(directory)
+        print(directory)
 
-for url in imageURLS:
-    r = requests.get(url)
+    for url in imageURLS:
+        r = requests.get(url)
 
-    # Create filename variable based on url
-    filename = url[url.rfind("/")+1:]
+        # Create filename variable based on url
+        filename = url[url.rfind("/")+1:]
 
-    if r.status_code == 200:
-        with open(directory + filename,'wb') as f:
-            f.write(r.content)
+        if r.status_code == 200:
+            with open(directory + filename,'wb') as f:
+                f.write(r.content)
+    return directory
 
 ###
 ### CREATE PDF
 ###
 
-# PDF file name setup, stripping trailing slash
-convertedFile = directory.rstrip("/") + ".pdf"
+def createPdf(directory):
+    # PDF file name setup, stripping trailing slash
+    convertedFile = directory.rstrip("/") + ".pdf"
 
-# Sorting images and creating a pdf with static width
-pdf = FPDF()
-for image in sorted(os.listdir(directory)):
-    pdf.add_page()
-    pdf.image(directory + image, w=185)
-pdf.output(convertedFile, "F")
-print(convertedFile)
+    # Sorting images and creating a pdf with static width
+    pdf = FPDF()
+    for image in sorted(os.listdir(directory)):
+        pdf.add_page()
+        pdf.image(directory + image, w=185)
+    pdf.output(convertedFile, "F")
+    print(convertedFile)
+    return convertedFile
 
 ###
 ### EMAIL
 ###
 
-mailRequest = requests.post(
-    mailUrl, 
-    auth=("api", mailKey),  
-    data={
-        "from": "postmaster@" + mailSandbox,
-        "to": mailRecipient,
-        "subject": "Incoming!",
-        "text": convertedFile
-    },
-    files={"attachment": (convertedFile, open(convertedFile, "rb"))},
-)
+def emailPdf(mailUrl, mailKey, mailSandbox, mailRecipient, convertedFile):
+    mailRequest = requests.post(
+        mailUrl, 
+        auth=("api", mailKey),  
+        data={
+            "from": "postmaster@" + mailSandbox,
+            "to": mailRecipient,
+            "subject": "Incoming!",
+            "text": convertedFile
+        },
+        files={"attachment": (convertedFile, open(convertedFile, "rb"))},
+    )
+    mailStatusCode = mailRequest.status_code
+    mailStatusText = mailRequest.text
+    return mailStatusCode, mailStatusText
 
-print(mailRequest.status_code)
-print(mailRequest.text)
+##
+## Execution
+##
+matchingURI, latestChapter = findLatest(domain, series)
+chapterURLList = findAllUrl(latestChapter, matchingURI)
+imageURLS = findUrl(chapterURLList)
+directory = downloadImages(latestChapter, series, imageURLS)
+convertedFile = createPdf(directory)
+mailStatusCode, mailStatusText = emailPdf(mailUrl, mailKey, mailSandbox, mailRecipient, convertedFile)
+print(mailStatusCode)
+print(mailStatusText)
 
 ##
 ## CLEANUP
